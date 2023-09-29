7e049e90d379a98d66f4616849c61f3e0ada458c
vukkaa <vuk121103@outlook.com>
2023-09-29 18:08:14 +0200

    Happiness Attribut wurde hinzugefügt und testen

--- test_app.py
+++ test_app.py
@@ -7,22 +7,36 @@ import pytest
 
 @pytest.fixture()
 def client():
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     client = app.test_client()
 
     yield client
 
 
-def test_add_entry(client):
-    # Test adding an entry
+# def test_add_entry(client):
+# Test adding an entry
+# response = client.post(
+#'/add_entry', data={'content': 'Test Entry Content'})
+
+# Check if the response is a redirect to the index page
+# assert response.status_code == 302
+# assert response.headers['Location'] == '/'
+
+# Check if the entry was added to the database
+# entry = entries[0]
+# assert entry is not None
+# assert entry.content == 'Test Entry Content'
+
+
+def test_add_entry_with_happiness(client):
     response = client.post(
-        '/add_entry', data={'content': 'Test Entry Content'})
+        "/add_entry", data={"content": "Test Entry Content", "happiness": "😄"}
+    )
 
-    # Check if the response is a redirect to the index page
     assert response.status_code == 302
-    assert response.headers['Location'] == '/'
+    assert response.headers["Location"] == "/"
 
-    # Check if the entry was added to the database
     entry = entries[0]
     assert entry is not None
-    assert entry.content == 'Test Entry Content'
+    assert entry.content == "Test Entry Content"
+    assert entry.happiness == "😄"  # neu
